9e9bf62fe391eeb3be684c4511c879ad2a82ce37
BDomzal <basiadomzal@gmail.com>
2026-03-14 16:36:54 +0100

    Fixed typo and cleaned file

--- default_parameters_search.py
+++ default_parameters_search.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ### Settings & imports
-
-# In[1]:
-
-
 import json
 
 with open('config.json', 'r') as f:
@@ -25,21 +17,11 @@ EXPERIMENTS_FOLDERS = config["experiments_folders"]
 GROUND_TRUTH_MOLAR_PROPORTIONS = config["ground_truth_molar_proportions"]
 
 
-# ### Experiments data
-
-# ### Improving heatmaps
-
-# In[2]:
-
-
-list_of_results_for_experiments = get_all_componets_results_concentrations(
+list_of_results_for_experiments = get_all_components_results_concentrations(
                                                                              EXPERIMENTS_FOLDERS, 
                                                                              GROUND_TRUTH_MOLAR_PROPORTIONS, 
                                                                              PROTONS_DICTIONARY
                                                                             )
-
-
-# In[3]:
 
 
 list_of_results_for_experiments_areas =  get_all_components_results_areas(
@@ -48,31 +30,13 @@ list_of_results_for_experiments_areas =  get_all_components_results_areas(
                                                                           PROTONS_DICTIONARY
                                                                          )
 
-
-# In[4]:
-
-
 total_error_all_experiments = get_total_error_from_all_experiments(list_of_results_for_experiments)
 
 
-# In[5]:
-
-
 draw_heatmap(total_error_all_experiments)
-
-
-# In[6]:
-
 
 a, b = total_error_all_experiments.stack().idxmin()
 print('Optimal kappa_mixture:')
 print(a)
 print('Optimal kappa_components:')
 print(b)
-
-
-# In[ ]:
-
-
-
-
